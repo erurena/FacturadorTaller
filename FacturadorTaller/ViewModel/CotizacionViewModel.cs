7775a96883e86f973255aee1b33fb4bfcb683d6a
erurena <erikaurena@hotmail.com>
2018-01-23 12:42:15 -0500

    sending changes

---
+++ FacturadorTaller/ViewModel/CotizacionViewModel.cs
@@ -0,0 +1,23 @@
+﻿using FacturadorTaller.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FacturadorTaller.ViewModel
+{
+    public class CotizacionViewModel
+    {
+        [ValidDate]
+        public string Fecha { get; set; }
+       
+        public Clientes Clientes { get; set; }
+        public Cotizacion Cotizacion { get; set; }
+        public IEnumerable<DetalleCot> DetalleCot { get; set; }
+
+        public DateTime GetDate()
+        {
+            return DateTime.Parse(string.Format(Fecha));
+        }
+    }
+}
---
+++ FacturadorTaller/ViewModel/ValidDate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+
+
+namespace FacturadorTaller.ViewModel
+{
+    public class ValidDate : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime dateTime;
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("es-MX");
+            var isValid = DateTime.TryParseExact(Convert.ToString(value),
+                "dd/MM/yyyy",
+                CultureInfo.CurrentCulture,
+                DateTimeStyles.None,
+                out dateTime);
+
+            return (isValid);
+        }
+    }
+}
